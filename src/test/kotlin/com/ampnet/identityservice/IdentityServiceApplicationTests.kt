bfa4e5fdf53dd3c4d6d4910c991fa7dc42591b52
Eugen Druzin <edruzin@gmail.com>
2021-06-16 12:46:36 +0200

    Add active profile test

--- src/test/kotlin/com/ampnet/identityservice/IdentityServiceApplicationTests.kt
+++ src/test/kotlin/com/ampnet/identityservice/IdentityServiceApplicationTests.kt
@@ -2,8 +2,10 @@ package com.ampnet.identityservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IdentityServiceApplicationTests {
 
     @Test
