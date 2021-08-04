351cd95bf3694a435da60d71fb4becb9dfb3ba64
Eugen Druzin <edruzin@gmail.com>
2021-08-04 12:36:40 +0200

    Fix mumbai id

--- src/main/kotlin/com/ampnet/identityservice/blockchain/Chain.kt
+++ src/main/kotlin/com/ampnet/identityservice/blockchain/Chain.kt
@@ -3,7 +3,7 @@ package com.ampnet.identityservice.blockchain
 @Suppress("MagicNumber")
 enum class Chain(val id: Long, val rpcUrls: List<String>, val infura: String) {
     MATIC_MAIN(137, listOf("https://rpc-mainnet.matic.network/"), "https://polygon-mainnet.infura.io/v3/"),
-    MATIC_TESTNET_MUMBAI(8001, listOf("https://rpc-mumbai.matic.today/"), "https://polygon-mumbai.infura.io/v3/"),
+    MATIC_TESTNET_MUMBAI(80001, listOf("https://rpc-mumbai.matic.today/"), "https://polygon-mumbai.infura.io/v3/"),
     ETHEREUM_MAIN(1, listOf("https://cloudflare-eth.com/"), "https://mainnet.infura.io/v3/");
 
     companion object {
