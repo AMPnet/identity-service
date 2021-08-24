d8d2066d17eaa53f65426dfbc1e72c858f0ce523
Eugen Druzin <edruzin@gmail.com>
2021-08-24 09:25:18 +0200

    Disable Pinata test

--- src/test/kotlin/com/ampnet/identityservice/service/PinataServiceTest.kt
+++ src/test/kotlin/com/ampnet/identityservice/service/PinataServiceTest.kt
@@ -2,9 +2,11 @@ package com.ampnet.identityservice.service
 
 import com.ampnet.identityservice.service.impl.PinataServiceImpl
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
 
+@Disabled("Not for automated testing")
 @ActiveProfiles("secret")
 class PinataServiceTest : JpaServiceTestBase() {
 
